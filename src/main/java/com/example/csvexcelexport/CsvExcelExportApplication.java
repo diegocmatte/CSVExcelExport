ee6e0249bb11517a13b8b7a093ed7f5da1ac65ff
Diego Matté <diegocmatte@gmail.com>
2022-07-16 13:58:19 -0300

    first commit

---
+++ src/main/java/com/example/csvexcelexport/CsvExcelExportApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvexcelexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvExcelExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvExcelExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csvexcelexport/CsvExcelExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvexcelexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvExcelExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
